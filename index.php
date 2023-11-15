d1806bf620dea071da477c838116983c70838f27
PabGarCas <35938477+PabGarCas@users.noreply.github.com>
2023-11-15 11:42:00 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+	<?php
+		$m = new mysqli('mariadb', 'conexion', 'QAtqSBvNefXNO1FX', 'test');
+		$r = $m->query("SELECT * FROM test");
+
+		echo "<table>";
+		echo "<tr><th>ID</th><th>Dato</th></tr>";
+		while($f = $r->fetch_array()) {
+			echo "<tr><th>".$f['id']."</th><th>'.$f['dato'].'</th></tr>";
+		}
+
+		echo "</table>";
+	?>
