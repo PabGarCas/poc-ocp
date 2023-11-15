0a8cd945db1df443e0ac26f4bb6b9e01b03ae7d3
PabGarCas <35938477+PabGarCas@users.noreply.github.com>
2023-11-15 11:51:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		echo "<table>";
 		echo "<tr><th>ID</th><th>Dato</th></tr>";
 		while($f = $r->fetch_array()) {
-			echo "<tr><th>".$f['id']."</th><th>'.$f['dato'].'</th></tr>";
+			echo "<tr><th>".$f['id']."</th><th>'.$f['dato'].'</th></tr>';
 		}
 
 		echo "</table>";
