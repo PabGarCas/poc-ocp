4723d6f6827976de87669f94ed3f40fee48087f9
PabGarCas <35938477+PabGarCas@users.noreply.github.com>
2023-11-15 14:06:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-	<?php
-		$m = new mysqli('mariadb', 'conexion', 'QAtqSBvNefXNO1FX', 'test');
-		$r = $m->query("SELECT * FROM acceso_nginx");
+<?php
+	if (!(isset($_ENV['DB_HOST'] && isset($_ENV['DB_USER'] && isset($_ENV['DB_PASS'] && isset($_ENV['DB_DATA'])) {
+		die("Variables de entorno no configuradas.");						      
+	}
+					      
+	$m = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_DATA'];
+	$r = $m->query("SELECT * FROM acceso_nginx");
 
-		echo "<table>";
-		echo "<tr><th>ID</th><th>Dato</th></tr>";
-		while($f = $r->fetch_array()) {
-			echo "<tr><th>".$f['id'].'</th><th>'.$f['dato'].'</th></tr>';
-		}
+	echo "<table border='1'>";
+	echo "<tr><th>ID</th><th>Dato</th></tr>";
+	while($f = $r->fetch_array()) {
+		echo "<tr><th>".$f['id'].'</th><th>'.$f['dato'].'</th></tr>';
+	}
 
-		echo "</table>";
-	?>
+	echo "</table>";
+?>
